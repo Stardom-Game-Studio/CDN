2277d71c0e0de69f9c74fbbc7f118b6dcc108a76
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2024-10-29 11:59:45 +0000

    Started work on /cdn

--- src/main.py
+++ src/main.py
@@ -2,9 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+content_paths = {
+    "audio1":"static/audio1"
+} # follows <cdn file name>:<static file path>
+
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return 'Ping!'
+
+@app.route('/cdn/get/<fileName>')
+def cdnGet(fileName):
+    fileName = str(fileName)
+    if fileName == "":
+        return "No file name", 401
+    # Get and return the file
+    
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=true)
